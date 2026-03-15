e7dfe33a297ed86c66f2c5ad7427e04d3db6cfdc
Advaita-Intelligence <info@advaita-tech.com>
2026-03-15 17:00:58 +0000

    Add files via upload

---
+++ acai-flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import acai_flutter
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AcaiFlutterPlugin.register(with: registry.registrar(forPlugin: "AcaiFlutterPlugin"))
+}
