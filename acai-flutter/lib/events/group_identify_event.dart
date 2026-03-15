254a8e0910161c56ccc8b6377f4057efe24d7f4d
Advaita-Intelligence <info@advaita-tech.com>
2026-03-15 16:56:06 +0000

    Add files via upload

---
+++ acai-flutter/lib/events/group_identify_event.dart
@@ -0,0 +1,6 @@
+import '../constants.dart';
+import 'base_event.dart';
+
+class GroupIdentifyEvent extends BaseEvent {
+  GroupIdentifyEvent() : super(Constants.groupIdentifyEvent);
+}
---
+++ acai-flutter/lib/events/revenue_event.dart
@@ -0,0 +1,6 @@
+import '../constants.dart';
+import 'base_event.dart';
+
+class RevenueEvent extends BaseEvent {
+  RevenueEvent() : super(Constants.revenueEvent);
+}
